401e9d2f8fd7c58bfe2f78d6ae74ff0ded16ec56
Keny Alejandro Serrato Ávila <kserratoa@gmail.com>
2024-04-13 21:11:38 -0500

    Prueba despliegue de endpoints v3

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,8 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuarioService } from './usuario.service';
 import { UsuarioController } from './usuario.controller';
+import { Usuario } from './entities/usuario.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Usuario])],
   controllers: [UsuarioController],
   providers: [UsuarioService],
 })
