08f6c4997e3dd6c4c481d7829c92566f81687755
Keny Alejandro Serrato Ávila <kserratoa@gmail.com>
2024-04-13 21:22:57 -0500

    Prueba Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000
+  const config = new DocumentBuilder()
+    .setTitle('API para la Gestión Documental y Seguimiento Académico de Proyectos Pedagógicos Integradores (PPI)')
+    .setDescription('La API de Gestión Documental y Seguimiento Académico de Proyectos Pedagógicos Integradores (PPI) del Politécnico Colombiano JIC proporciona un conjunto de endpoints diseñados para facilitar la administración y seguimiento de los PPIs en el contexto de la Técnica y Tecnología en Sistematización de Datos. Esta API permite a los usuarios acceder, crear, actualizar y eliminar información relacionada con los documentos, estudiantes, docentes y actividades académicas asociadas a los PPIs.')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  const port = process.env.PORT || 3000;
+  SwaggerModule.setup('api', app, document);
   await app.listen(port);
 }
 bootstrap();
