92a5d0b83219fcc29ac6cd1959c5c0b7fb87334b
Keny Alejandro Serrato Ávila <kserratoa@gmail.com>
2024-04-17 10:45:28 -0500

    Prueba 9 Auth Google

--- src/usuario/dto/email.dto.ts
+++ src/usuario/dto/email.dto.ts
@@ -1,11 +1,11 @@
 /* eslint-disable prettier/prettier */
-import { IsNotEmpty, IsEmail } from 'class-validator';
-//import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsEmail, Matches } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class EmailDTO {
   @IsNotEmpty({ message: 'El correo electrónico es obligatorio' })
   @IsEmail({}, { message: 'El formato del correo electrónico es inválido' })
-  //@Transform(({ value }) => value.toLowerCase())
-  //@Matches(/@elpoli\.edu\.co$/, { message: 'El correo no pertenece al dominio del PJIC' })
+  @Transform(({ value }) => value.toLowerCase())
+  @Matches(/@elpoli\.edu\.co$/, { message: 'El correo no pertenece al dominio del PJIC' })
   email: string;
 }
