3d56cc29cdb1492e076f399763d7972818473730
Keny Alejandro Serrato Ávila <kserratoa@gmail.com>
2024-04-17 10:59:27 -0500

    Prueba CORS

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,26 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const config = new DocumentBuilder()
     .setTitle('API para la Gestión Documental y Seguimiento Académico de Proyectos Pedagógicos Integradores (PPI)')
     .setDescription('La API de Gestión Documental y Seguimiento Académico de Proyectos Pedagógicos Integradores (PPI) del Politécnico Colombiano JIC proporciona un conjunto de endpoints diseñados para facilitar la administración y seguimiento de los PPIs en el contexto de la Técnica y Tecnología en Sistematización de Datos. Esta API permite a los usuarios acceder, crear, actualizar y eliminar información relacionada con los documentos, estudiantes, docentes y actividades académicas asociadas a los PPIs.')
     .setVersion('1.0')
     .addTag('API')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
+
+  app.enableCors({
+    allowedHeaders: "*",
+    origin: "*",
+  });
+
   app.useGlobalPipes(new ValidationPipe());
+
   const port = process.env.PORT || 3000;
   SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 }
 bootstrap();
