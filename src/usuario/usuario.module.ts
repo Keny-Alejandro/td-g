63075f8bb3bb9e61241838a2b6117a45ece9b37d
Keny Alejandro Serrato Ávila <kserratoa@gmail.com>
2024-04-29 15:57:05 -0500

    Prueba 3 Carga Excel

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -4,9 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuarioService } from './usuario.service';
 import { UsuarioController } from './usuario.controller';
 import { Usuario } from './entities/usuario.entity';
+import { Programa } from 'src/programa/entities/programa.entity';
+import { Rol } from 'src/rol/entities/rol.entity';
+import { Asignatura } from 'src/asignatura/entities/asignatura.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Usuario])],
+  imports: [
+    TypeOrmModule.forFeature([Usuario, Programa, Rol, Asignatura]),
+  ],
   controllers: [UsuarioController],
   providers: [UsuarioService],
 })
