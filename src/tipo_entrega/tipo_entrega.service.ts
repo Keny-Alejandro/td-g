a0f2f8a14ae3a4f156ef600a3739065666069823
Keny Alejandro Serrato Ávila <kserratoa@gmail.com>
2024-05-16 01:19:35 -0500

    Nueva prueba AM

--- src/tipo_entrega/tipo_entrega.service.ts
+++ src/tipo_entrega/tipo_entrega.service.ts
@@ -12,6 +12,15 @@ export class TipoEntregaService {
   ) {}
 
   async findAll() {
-    return this.tipoEntregaRepository.find();
+    return this.tipoEntregaRepository.query(`
+    SELECT
+      te."Tipo_Entrega_ID" as id,
+      te."Tipo_Entrega_Descripcion" as nombre,
+      ce."Porcentaje_Entrega"
+    FROM
+      "Tipo_Entrega" te
+    INNER JOIN "Configuracion_Entrega" ce ON
+      ce."Tipo_Entrega_ID" = te."Tipo_Entrega_ID"
+  `);
   }
 }
