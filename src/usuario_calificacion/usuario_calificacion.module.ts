92404c7c6909e79630afd2e176529e232608fc87
Keny Alejandro Serrato Ávila <kserratoa@gmail.com>
2024-05-21 22:19:47 -0500

    little fix

--- src/usuario_calificacion/usuario_calificacion.module.ts
+++ src/usuario_calificacion/usuario_calificacion.module.ts
@@ -1,8 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuarioCalificacionService } from './usuario_calificacion.service';
 import { UsuarioCalificacionController } from './usuario_calificacion.controller';
+import { UsuarioCalificacion } from './entities/usuario_calificacion.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UsuarioCalificacion])],
   controllers: [UsuarioCalificacionController],
   providers: [UsuarioCalificacionService],
 })
