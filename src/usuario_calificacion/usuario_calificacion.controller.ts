522d106d827b70bc1ec04ecc98552a313731ff92
Keny Alejandro Serrato Ávila <kserratoa@gmail.com>
2024-05-22 03:43:49 -0500

    actualizar calificacion individual

--- src/usuario_calificacion/usuario_calificacion.controller.ts
+++ src/usuario_calificacion/usuario_calificacion.controller.ts
@@ -1,22 +1,35 @@
 /* eslint-disable prettier/prettier */
-import {
-  Controller,
-  Get,
-  Param,
-} from '@nestjs/common';
-import { UsuarioCalificacionService } from './usuario_calificacion.service';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UsuarioCalificacion } from './entities/usuario_calificacion.entity';
+import { UsuarioCalificacionService } from './usuario_calificacion.service';
 
 @ApiTags('usuario-calificacion')
 @Controller('usuario-calificacion')
 export class UsuarioCalificacionController {
   constructor(
     private readonly usuarioCalificacionService: UsuarioCalificacionService,
-  ) { }
+  ) {}
 
   @Get(':user')
-  async getCalificacionesByUser(@Param('user') user: number): Promise<UsuarioCalificacion[]> {
+  async getCalificacionesByUser(
+    @Param('user') user: number,
+  ): Promise<UsuarioCalificacion[]> {
     return await this.usuarioCalificacionService.findByUser(user);
+  }
+
+  @Post('actualizar-calificaciones')
+  async actualizarCalificaciones(
+    @Body() calificaciones: UsuarioCalificacion[],
+  ): Promise<string> {
+    try {
+      await this.usuarioCalificacionService.actualizarCalificaciones(
+        calificaciones,
+      );
+      return 'Éxito en la calificación.';
+    } catch (error) {
+      console.error('Error al actualizar calificaciones:', error);
+      throw new Error('Ocurrió un error al actualizar las calificaciones');
+    }
   }
 }
--- src/usuario_calificacion/usuario_calificacion.service.ts
+++ src/usuario_calificacion/usuario_calificacion.service.ts
@@ -6,13 +6,34 @@ import { UsuarioCalificacion } from './entities/usuario_calificacion.entity';
 
 @Injectable()
 export class UsuarioCalificacionService {
-
   constructor(
     @InjectRepository(UsuarioCalificacion)
     private readonly usuarioCalificacionRepository: Repository<UsuarioCalificacion>,
-  ) { }
+  ) {}
 
   async findByUser(user: number): Promise<UsuarioCalificacion[]> {
     return await this.usuarioCalificacionRepository.find({ where: { user } });
   }
+
+  async actualizarCalificaciones(calificaciones: UsuarioCalificacion[]): Promise<void> {
+    try {
+      for (const calificacion of calificaciones) {
+        const { user, entrega, calificacion: calificacionValor } = calificacion;
+        let usuarioCalificacion = await this.usuarioCalificacionRepository.findOne({ where: { user, entrega } });
+  
+        if (!usuarioCalificacion) {
+          usuarioCalificacion = new UsuarioCalificacion();
+          usuarioCalificacion.user = user;
+          usuarioCalificacion.entrega = entrega;
+        }
+  
+        usuarioCalificacion.calificacion = calificacionValor;
+        await this.usuarioCalificacionRepository.save(usuarioCalificacion);
+      }
+    } catch (error) {
+      console.error('Error al actualizar calificaciones:', error);
+      throw new Error('Ocurrió un error al actualizar las calificaciones');
+    }
+  }
+  
 }
