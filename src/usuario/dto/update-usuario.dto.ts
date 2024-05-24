0569c7a795ef1c113b5f2ac5df2e70a8fc9b0933
Alejandro Bernal <alejandrobernalduque8@gmail.com>
2024-05-24 00:57:35 -0500

    CrearAsesoresBack 9.0

--- src/usuario/dto/update-usuario.dto.ts
+++ src/usuario/dto/update-usuario.dto.ts
@@ -1,8 +1,8 @@
 /* eslint-disable prettier/prettier */
 export class UpdateUsuarioDTO {
-    Usuario_ID: number;
-    Rol_ID: number ; // Representa el ID del rol asociado al usuario
-    Usuario_Nombre: string;
-    Usuario_Documento: string;
-    Usuario_Correo: string;
+    id: number;
+    rol: number ; // Representa el ID del rol asociado al usuario
+    nombre: string;
+    documento: string;
+    correo: string;
 }
