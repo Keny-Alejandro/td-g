784432a9a05f0c14a02448e3fde4367af3c38d5b
Alejandro Bernal <alejandrobernalduque8@gmail.com>
2024-05-27 02:41:13 -0500

    fix module - usuario_asignatura

--- src/usuario_asignatura/usuario_asignatura.module.ts
+++ src/usuario_asignatura/usuario_asignatura.module.ts
@@ -4,10 +4,11 @@ import { UsuarioAsignaturaService } from './usuario_asignatura.service';
 import { UsuarioAsignaturaController } from './usuario_asignatura.controller';
 import { UsuarioAsignatura } from './entities/usuario_asignatura.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from '../usuario/entities/usuario.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UsuarioAsignatura]),
+    TypeOrmModule.forFeature([UsuarioAsignatura, Usuario]),
   ],
   controllers: [UsuarioAsignaturaController],
   providers: [UsuarioAsignaturaService],
